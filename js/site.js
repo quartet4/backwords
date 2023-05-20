e1839ed0a04d7edf583cb0816e9724e877c3508d
Jonathan Mendoza <quartet4@mac.com>
2023-05-20 16:47:31 -0400

    Update all pages and js code

--- js/site.js
+++ js/site.js
@@ -1,4 +1,34 @@
-// Call Hello World
-function HelloWorld() {
-  alert("Hello World!");
+// Get string from the page
+// Controller
+function getValue() {
+  document.getElementById("alert").classList.add("invisible");
+
+  let userString = document.getElementById("userString").value;
+
+  let revString = reverseString(userString);
+
+  displayString(revString);
+}
+
+// Reverse the string
+// Logic
+function reverseString(userString) {
+  let revString = [];
+
+  // reverse string using a for loop
+  for (let i = userString.length - 1; i >= 0; i--) {
+    revString += userString[i];
+  }
+
+  return revString;
+}
+
+// Display the reversed string to the user
+// View
+function displayString(revString) {
+  // write to the page
+  document.getElementById("msg").innerHTML = `<strong>${revString}</strong>`;
+
+  // show the alert box
+  document.getElementById("alert").classList.remove("invisible");
 }
